2aeac509ff4a14a7da290f204cd07f4c5cd62e0d
Siddharth <sidp5276@gmail.com>
2025-12-21 16:20:44 +0530

    init commit

---
+++ src/Task/Lab002_Printf_table.java
@@ -0,0 +1,16 @@
+package Task;
+public class Lab002_Printf_table {
+    public static void main(String[] args){
+            byte a = 9;
+            System.out.printf("table of %d  1 = %d%n", a, a*1);
+            System.out.printf("table of %d  1 = %d%n", a, a*2);
+            System.out.printf("table of %d  1 = %d%n", a, a*3);
+            System.out.printf("table of %d  1 = %d%n", a, a*4);
+            System.out.printf("table of %d  1 = %d%n", a, a*5);
+            System.out.printf("table of %d  1 = %d%n", a, a*6);
+            System.out.printf("table of %d  1 = %d%n", a, a*7);
+            System.out.printf("table of %d  1 = %d%n", a, a*8);
+            System.out.printf("table of %d  1 = %d%n", a, a*9);
+            System.out.printf("table of %d  1 = %d%n", a, a*10);
+        }
+    }
