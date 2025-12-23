3ee7301752c8cc5a77f2c95bf8f568b2f69dd4a3
Siddharth <sidp5276@gmail.com>
2025-12-23 13:56:22 +0530

    init commit

---
+++ src/ex_01_Java_Basics/Eveodd.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basics;
+
+import java.util.Scanner;
+public class Eveodd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a % 2 == 0) {
+            System.out.println(a + " is even number");
+        } else {
+            System.out.println(a + " is an odd number");
+        }
+        sc.close();
+    }
+}
