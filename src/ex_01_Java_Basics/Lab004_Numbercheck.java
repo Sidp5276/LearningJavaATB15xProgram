afc8e1ac3ac9bbc9bbc931aa34da7c7fdf73932b
Siddharth <sidp5276@gmail.com>
2025-12-23 15:12:27 +0530

    init commit

---
+++ src/ex_01_Java_Basics/Lab004_Numbercheck.java
@@ -0,0 +1,20 @@
+package ex_01_Java_Basics;
+//Check if a number is divided by 5.
+
+import java.util.Scanner;
+
+ public class Lab004_Numbercheck{
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter a number: ");
+            int n = scanner.nextInt();
+            int remainder = n % 5;
+            if (remainder == 0) {
+                System.out.println(n + " is divisible by 5");
+            } else  {
+                System.out.println(n + " is not divisible by 5");
+                System.out.print("remainder is : " + remainder);
+            }
+            scanner.close();
+        }
+    }
