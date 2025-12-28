0958bf441fc68afd27e69a3c8d23bdacaee656bc
Siddharth <sidp5276@gmail.com>
2025-12-28 18:10:42 +0530

    init commit

---
+++ src/ex_03_Literals/Lab_32_Non_Primitive_Literals.java
@@ -0,0 +1,11 @@
+package ex_03_Literals;
+
+public class Lab_32_Non_Primitive_Literals {
+    public static void main(String[] args) {
+        String name = "Sid"; // String is bunch of char.
+        int[] arrays_of_items = new int[10];
+
+        char A = '0';
+        System.out.println(A);
+    }
+}
