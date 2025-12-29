9e999ff98f732b3d33ec1b6eff570c2924e036cc
Siddharth <sidp5276@gmail.com>
2025-12-29 20:59:46 +0530

    init commit

---
+++ src/ex_04_Operators/Lab_39_Interview_Concat.java
@@ -0,0 +1,22 @@
+package ex_04_Operators;
+
+public class Lab_39_Interview_Concat {
+    public static void main(String[] args) {
+        // +
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+
+        // + -> String -> Concatenation
+        String first_name = "Pramod";
+        String last_name = "Dutta";
+        System.out.println(first_name+last_name); // concatenation
+
+//        System.out.println(first_name-last_name);
+//        System.out.println(first_name*last_name);
+//        System.out.println(first_name/last_name);
+
+        // + - behave arch int
+        // + - concat String
+    }
+}
