344c5467efa9a36092ad7834d6cf4097e6d8c1b8
Siddharth <sidp5276@gmail.com>
2025-12-30 19:50:12 +0530

    init commit

---
+++ src/ex_04_Operators/Lab_47_New_Operators.java
@@ -0,0 +1,10 @@
+package ex_04_Operators;
+
+public class Lab_47_New_Operators {
+    public static void main(String[] args) {
+        // New Operator
+        // Non Primitive variables
+        String name = new String("Pramod");
+        // Learn in the OOPs
+    }
+}
