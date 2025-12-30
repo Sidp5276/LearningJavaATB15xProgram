9f5ae66e75c55f26868716e44020fa8150ace3eb
Siddharth <sidp5276@gmail.com>
2025-12-30 19:46:44 +0530

    init commit

---
+++ src/ex_04_Operators/Lab_43_Interview.java
@@ -0,0 +1,9 @@
+package ex_04_Operators;
+
+public class Lab_43_Interview {
+    public static void main(String[] args)
+    {
+        // === // this is nothing
+        System.out.println(!(10>20));
+    }
+}
