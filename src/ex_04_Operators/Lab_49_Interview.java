b8582940b6bee43f4bbbddd1f5f5e97b1a821747
Siddharth <sidp5276@gmail.com>
2025-12-30 19:57:08 +0530

    init commit

---
+++ src/ex_04_Operators/Lab_49_Interview.java
@@ -0,0 +1,14 @@
+package ex_04_Operators;
+
+public class Lab_49_Interview {
+    public static void main(String[] args) {
+        int a = 10;
+        boolean b = (10 == 11);
+        System.out.println(b);
+    }
+
+
+            long l = 100l;
+            // How Bytes, Bits ?
+            //  8 Bytes, 64 Bits.
+        }
