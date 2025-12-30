e55181c107cc1a07347726233ff8d0e97323803d
Siddharth <sidp5276@gmail.com>
2025-12-30 19:51:23 +0530

    init commit

---
+++ src/ex_04_Operators/Lab_48_Instanceof_Operators.java
@@ -0,0 +1,9 @@
+package ex_04_Operators;
+
+public class Lab_48_Instanceof_Operators {
+    public static void main(String[] args) {
+        String s1 = new String("Sid");
+        int a = 10;
+        System.out.println(s1 instanceof String);
+    }
+}
