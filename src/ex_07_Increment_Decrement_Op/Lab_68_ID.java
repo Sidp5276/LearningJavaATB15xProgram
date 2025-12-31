ba487d27d34ab5a098aace9eaca057562461224f
Siddharth <sidp5276@gmail.com>
2025-12-31 19:03:19 +0530

    init commit

---
+++ src/ex_07_Increment_Decrement_Op/Lab_68_ID.java
@@ -0,0 +1,9 @@
+package ex_07_Increment_Decrement_Op;
+
+public class Lab_68_ID {
+    public static void main(String[] args) {
+        int a = 10;
+        int result = ++a;
+        System.out.println(result);
+    }
+}
