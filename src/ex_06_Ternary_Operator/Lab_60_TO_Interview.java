df499129e48b17a4140e147a742aba7b73765c2c
Siddharth <sidp5276@gmail.com>
2025-12-31 18:44:46 +0530

    init commit

---
+++ src/ex_06_Ternary_Operator/Lab_60_TO_Interview.java
@@ -0,0 +1,8 @@
+package ex_06_Ternary_Operator;
+
+public class Lab_60_TO_Interview {
+    public static void main(String[] args) {
+        int amol_age = 21;
+        String result = amol_age > 18 ? (amol_age > 25 ? "You can drink": "You can't") : "No you can't go to GOA";
+    }
+}
