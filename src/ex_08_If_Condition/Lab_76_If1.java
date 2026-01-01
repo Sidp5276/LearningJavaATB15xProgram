376255dcc2eb2d28e7ca9f717ac9474ced007d11
Siddharth <sidp5276@gmail.com>
2026-01-01 19:56:53 +0530

    init commit

---
+++ src/ex_08_If_Condition/Lab_76_If1.java
@@ -0,0 +1,10 @@
+package ex_08_If_Condition;
+
+public class Lab_76_If1 {
+    public static void main(String[] args) {
+        int age = 17;
+        if(age > 18){
+            System.out.println("Yes");
+        }
+    }
+}
