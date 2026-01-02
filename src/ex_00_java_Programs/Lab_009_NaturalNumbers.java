e0dd1788894bb8a4dbdbd43d9d1fd4ca0172c237
Siddharth <sidp5276@gmail.com>
2026-01-02 18:30:42 +0530

    2nd jan

---
+++ src/ex_00_java_Programs/Lab_009_NaturalNumbers.java
@@ -0,0 +1,11 @@
+package ex_00_java_Programs;
+
+public class Lab_009_NaturalNumbers {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
