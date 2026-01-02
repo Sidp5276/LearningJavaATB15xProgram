fe7a723048e309fb1486e7410f1a85e51cfa5df5
Siddharth <sidp5276@gmail.com>
2026-01-02 18:29:48 +0530

    2nd jan

---
+++ src/ex_00_java_Programs/Lab_008_Loops.java
@@ -0,0 +1,11 @@
+package ex_00_java_Programs;
+
+public class Lab_008_Loops {
+public static void main(String[] args) {
+    int i;
+    for (i = 1; i <= 10; i++)
+    {
+        System.out.println("hey");
+    }
+}
+}
