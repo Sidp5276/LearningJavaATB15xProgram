a11de060ece5498c1a1bb906a194c6c48918a205
Siddharth <sidp5276@gmail.com>
2026-01-05 11:21:36 +0530

    5th jan

---
+++ src/ex_10_For_Loop/Lab_98_Fot_IQ_P1.java
@@ -0,0 +1,9 @@
+package ex_10_For_Loop;
+
+public class Lab_98_Fot_IQ_P1 {
+    public static void main(String[] args) {
+        for (int i = 0;i>10 ;) {
+            System.out.println("Hello");
+        }
+    }
+}
