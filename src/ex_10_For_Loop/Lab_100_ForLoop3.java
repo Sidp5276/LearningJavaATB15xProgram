00c7c027fb2c26e0f5dbbd929a1a0ab737809de5
Siddharth <sidp5276@gmail.com>
2026-01-05 11:24:20 +0530

    5th jan

---
+++ src/ex_10_For_Loop/Lab_100_ForLoop3.java
@@ -0,0 +1,10 @@
+package ex_10_For_Loop;
+
+public class Lab_100_ForLoop3 {
+    public static void main(String[] args) {
+        int i = 10;
+        for (; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
