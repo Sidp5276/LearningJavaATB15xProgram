fdb563f4ec23c2bea0497619f153c9d4f88005e8
Siddharth <sidp5276@gmail.com>
2026-01-05 11:41:34 +0530

    5th jan

---
+++ src/ex_10_For_Loop/Lab_102_oddeven_From50.java
@@ -0,0 +1,15 @@
+package ex_10_For_Loop;
+
+public class Lab_102_oddeven_From50 {
+    public static void main(String[] args) {
+        // TO find the even numbers from 1 to 50
+
+        for (int i = 1; i <= 50; i++) {
+//            if (i % 2 == 0) {
+            if (i % 2 != 0) {
+//                System.out.println("Even => " + i);
+                System.out.println("Odd = " + i);
+            }
+        }
+    }
+}
