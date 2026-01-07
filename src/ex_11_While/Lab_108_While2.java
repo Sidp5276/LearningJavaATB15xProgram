b6578be42f0e473f682ec2f26d27b4765f67780b
Siddharth <sidp5276@gmail.com>
2026-01-07 17:56:55 +0530

    7th jan

---
+++ src/ex_11_While/Lab_108_While2.java
@@ -0,0 +1,11 @@
+package ex_11_While;
+
+public class Lab_108_While2 {
+    public static void main(String[] args) {
+        int xyz = 1;
+        while (xyz <= 15) {
+            System.out.println("xyz will do 15 years");
+            xyz++;
+        }
+    }
+}
