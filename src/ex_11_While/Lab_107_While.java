b298f2fe7932faa4abeae486f7c18a8d222a070b
Siddharth <sidp5276@gmail.com>
2026-01-07 17:55:14 +0530

    7th jan

---
+++ src/ex_11_While/Lab_107_While.java
@@ -0,0 +1,17 @@
+package ex_11_While;
+
+public class Lab_107_While {
+    public static void main(String[] args) {
+
+        int i = 0; // Init
+        while (i < 10) {//condition
+            System.out.print(i + "\t");
+            i++; // Updation
+        }
+
+        //  I, C, U
+        for (int j = 0; j < 10 ; j++) {
+            System.out.println(j);
+        }
+    }
+}
