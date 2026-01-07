3bb55695d9155988b58e3847ddeb8235bb036306
Siddharth <sidp5276@gmail.com>
2026-01-07 17:58:39 +0530

    7th jan

---
+++ src/ex_11_While/Lab_110_While.java
@@ -0,0 +1,9 @@
+package ex_11_While;
+
+public class Lab_110_While {
+    public static void main(String[] args) {
+        while (true){
+            System.out.println("Hello, Pramod");
+        }
+    }
+}
