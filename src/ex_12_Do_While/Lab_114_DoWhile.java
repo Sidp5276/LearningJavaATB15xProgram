ebdd6d5156ba05b049aec64ee2bcb6d42c73c9b9
Siddharth <sidp5276@gmail.com>
2026-01-09 22:41:43 +0530

    9th jan

---
+++ src/ex_12_Do_While/Lab_114_DoWhile.java
@@ -0,0 +1,11 @@
+package ex_12_Do_While;
+
+public class Lab_114_DoWhile {
+    public static void main(String[] args) {
+        int a = 1;
+        do{
+            System.out.println(a);
+            a++;
+        }while (a<10);
+    }
+}
