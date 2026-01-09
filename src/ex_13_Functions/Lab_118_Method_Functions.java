e2f97d2b6260018edd0104547f804ce4e91d85f3
Siddharth <sidp5276@gmail.com>
2026-01-09 22:49:24 +0530

    9th jan

---
+++ src/ex_13_Functions/Lab_118_Method_Functions.java
@@ -0,0 +1,11 @@
+package ex_13_Functions;
+
+public class Lab_118_Method_Functions {
+    static void name_of_function(){
+        System.out.println("Hi");
+    }
+
+    public static void main(String[] args) {
+        name_of_function();
+    }
+}
