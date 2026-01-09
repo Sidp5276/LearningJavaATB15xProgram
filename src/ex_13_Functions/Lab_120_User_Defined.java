9d59031e5e84bec6ab849e7485523ebc488f9c1b
Siddharth <sidp5276@gmail.com>
2026-01-09 22:52:16 +0530

    9th jan

---
+++ src/ex_13_Functions/Lab_120_User_Defined.java
@@ -0,0 +1,23 @@
+package ex_13_Functions;
+import java.util.Scanner;
+
+public class Lab_120_User_Defined {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number a=");
+        int a = scanner.nextInt();
+        System.out.println("Enter a number b=");
+        int b = scanner.nextInt();
+        int sum = sum_two_numbers(a,b);
+        System.out.println("Sum of the number os = "+sum);
+
+    }
+
+    static int sum_two_numbers(int x,int y){
+        return x+y;
+    }
+    static void sum_of_two_number(){
+        System.out.println("Hi,there");
+    }
+}
