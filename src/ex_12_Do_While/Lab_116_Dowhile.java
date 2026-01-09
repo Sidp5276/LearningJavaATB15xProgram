dd5c96cbb6ac70d20b0a2052752dbe24088e96da
Siddharth <sidp5276@gmail.com>
2026-01-09 22:44:01 +0530

    9th jan

---
+++ src/ex_12_Do_While/Lab_116_Dowhile.java
@@ -0,0 +1,14 @@
+package ex_12_Do_While;
+
+public class Lab_116_Dowhile {
+    public static void main(String[] args) {
+        int number = 0;
+        // 0 -> it can be 0 to 10 - elements or students
+        do{
+            System.out.println(number);
+            // callFunction(number);
+            // close the excel file, open the file.
+            number++;
+        }while (number < 0);
+    }
+}
