a9736d8e5ca93a47bf98ea104e597cc73a63007c
Siddharth <sidp5276@gmail.com>
2026-01-11 21:47:20 +0530

    do while even

---
+++ src/TEST/Q10_DoWhileEven.java
@@ -0,0 +1,15 @@
+package TEST;
+//Write a Java program to print all even numbers between 1 and 50 using a do-while loop.
+
+public class Q10_DoWhileEven {
+    public static void main(String[] args) {
+        int i = 2;
+
+
+        do {
+            System.out.println(i);
+            i += 2;
+        } while (i <= 50);
+    }
+}
+
