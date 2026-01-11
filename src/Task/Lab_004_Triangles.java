c6f99942b20bda37fe59add6172d466235524b72
Siddharth <sidp5276@gmail.com>
2026-01-11 17:40:57 +0530

    Triangles

--- src/Task/Lab_004_Triangles.java
+++ src/Task/Lab_004_Triangles.java
@@ -1,15 +1,26 @@
 package Task;
+
 import java.util.Scanner;
+
 public class Lab_004_Triangles{
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter Side 1:");
+
         int side1 = scanner.nextInt();
+
         System.out.println("Enter Side 2:");
+
         int side2 = scanner.nextInt();
+
         System.out.println("Enter Side 3:");
+
         int side3 = scanner.nextInt();
+
         // Triangle validity check
+
         if (side1 + side2 <= side3 ||
                 side1 + side3 <= side2 ||
                 side2 + side3 <= side1) {
