a8defb6dab5cf6923c4fee2ce52efafc8fd7300e
Siddharth <sidp5276@gmail.com>
2026-01-14 12:31:54 +0530

    string append

---
+++ src/ex_15_String_Buffer/Lab_137_Buffer1.java
@@ -0,0 +1,9 @@
+package ex_15_String_Buffer;
+
+public class Lab_137_Buffer1 {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder("Siddharth");
+        stringBuilder.append(123);
+        System.out.println(stringBuilder);
+    }
+}
