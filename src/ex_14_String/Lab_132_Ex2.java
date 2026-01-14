6e8bcfed9cf82b613d0d4bf46c5122ffe01245d4
Siddharth <sidp5276@gmail.com>
2026-01-14 12:19:10 +0530

    string Example2

---
+++ src/ex_14_String/Lab_132_Ex2.java
@@ -0,0 +1,9 @@
+package ex_14_String;
+
+public class Lab_132_Ex2 {
+    public static void main(String[] args) {
+        CharSequence s = "Abcdefgh";
+        System.out.println(s);
+        System.out.println(s.subSequence(1,4));
+    }
+}
