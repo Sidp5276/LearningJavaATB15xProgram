d0f6cb0df1c8a4092b6f97853db47d01f27b7e3b
Siddharth <sidp5276@gmail.com>
2026-01-14 12:10:05 +0530

    string concat

---
+++ src/ex_14_String/Lab_125_String_Concat.java
@@ -0,0 +1,10 @@
+package ex_14_String;
+
+public class Lab_125_String_Concat {
+    public static void main(String[] args) {
+            String s1 = "hello";
+            s1 = s1.concat("world");
+            System.out.println(s1);
+
+        }
+    }
