909b0e02b10c54ef7cacd44c1c40fd8f016d2abf
Siddharth <sidp5276@gmail.com>
2026-01-19 21:18:28 +0530

    Arrays

---
+++ src/ex_16_Arrays/Lab_144_Arrays.java
@@ -0,0 +1,16 @@
+package ex_16_Arrays;
+
+public class Lab_144_Arrays {
+    public static void main(String[] args) {
+
+        String[] name_atb12x = new String[3];
+
+        name_atb12x[0] = "adam";
+        name_atb12x[1] = "Joy";
+        name_atb12x[2] = "aaron";
+
+        System.out.println(name_atb12x[0]);
+        System.out.println(name_atb12x[2]);
+        System.out.println(name_atb12x[1]);
+    }
+}
