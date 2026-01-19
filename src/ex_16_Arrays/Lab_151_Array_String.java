c091e0bc99905c94efb6c223da48ce3b59c5798f
Siddharth <sidp5276@gmail.com>
2026-01-19 22:12:02 +0530

    .

---
+++ src/ex_16_Arrays/Lab_151_Array_String.java
@@ -0,0 +1,9 @@
+package ex_16_Arrays;
+
+public class Lab_151_Array_String {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
