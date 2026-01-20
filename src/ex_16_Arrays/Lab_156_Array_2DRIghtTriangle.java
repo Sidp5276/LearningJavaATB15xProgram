9fa0d783eab39801caf71a698e41d1d522958737
Siddharth <sidp5276@gmail.com>
2026-01-20 16:21:57 +0530

    Array right triangle

---
+++ src/ex_16_Arrays/Lab_156_Array_2DRIghtTriangle.java
@@ -0,0 +1,16 @@
+package ex_16_Arrays;
+import java.util.Scanner;
+public class Lab_156_Array_2DRIghtTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the n for the pattern, e.g n = 3");
+        int n = scanner.nextInt(); // 3
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+
+        }
+    }
+}
