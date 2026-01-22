0ed1ac90153ae31bf44928b48244829432bb9053
Siddharth <sidp5276@gmail.com>
2026-01-22 19:07:58 +0530

    OOPs

---
+++ src/ex_17_OOPs/Lab_164_OOPs.java
@@ -0,0 +1,18 @@
+package ex_17_OOPs;
+
+public class Lab_164_OOPs {
+    public static void main(String[] args) {
+
+        Person p1 = new Person();
+        p1.name = "    ";
+
+
+        Person p2 = new Person();
+        p2.name = "    ";
+
+
+        Person p3;
+
+        new Person();
+    }
+    }
