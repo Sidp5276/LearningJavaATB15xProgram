7b4fc794d3d1bd97e95e1db53ef181aeee1c16a7
Siddharth <sidp5276@gmail.com>
2026-01-22 19:02:39 +0530

    oops attributes

---
+++ src/ex_17_OOPs/Lab_161_OOPs.java
@@ -0,0 +1,19 @@
+package ex_17_OOPs;
+
+public class Lab_161_OOPs {
+    // Attributes
+    String name;
+    int age;
+    String gender;
+    int salary;
+    long phone_no;
+    String address;
+    String courseName;
+    String paymentID;
+    // Behaviour
+    void speak(){}
+    void eat(){}
+    void sleep(){};
+    void doAssignment(){};
+    void watchRecordings(){};
+}
