3257efab452c877898e68bb0ffc79b007a427df5
Siddharth <sidp5276@gmail.com>
2026-02-09 21:34:44 +0530

    Enum web auto

---
+++ src/ex_22_ENUM/Lab_181_Enum_Web_Automation.java
@@ -0,0 +1,7 @@
+package ex_22_ENUM;
+
+public class Lab_181_Enum_Web_Automation {
+    public static void main(String[] args) {
+        System.out.println(Locators.page_input_email.getLocator());
+    }
+}
