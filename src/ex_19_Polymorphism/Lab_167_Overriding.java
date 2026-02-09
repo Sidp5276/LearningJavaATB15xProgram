2aff4d2f0812d0e96bb91753bf8b983a1d48255b
Siddharth <sidp5276@gmail.com>
2026-02-09 20:48:34 +0530

    overriding

---
+++ src/ex_19_Polymorphism/Lab_167_Overriding.java
@@ -0,0 +1,20 @@
+package ex_19_Polymorphism;
+
+public class Lab_167_Overriding {
+    public static void main(String[] args) {
+      /*  Pramod p1 = new Pramod();
+        p1.home();
+
+        Father f1 = new Father();
+        f1.home();
+
+        Father f2 = new Pramod(); // Dynamic Dispatch /
+        f2.home();
+
+
+        //  Pramod p1 = new Father();
+        // When father is getting born, child reference cannot be given to.
+
+       */
+    }
+}
