4b069535e238abb677a984a8c356cb10557baad1
Siddharth <sidp5276@gmail.com>
2026-02-09 21:28:43 +0530

    ENUM P3

---
+++ src/ex_22_ENUM/Lab_178_ENUM_P3.java
@@ -0,0 +1,27 @@
+package ex_22_ENUM;
+
+public class Lab_178_ENUM_P3 {
+    public static void main(String[] args) {
+        System.out.println(HEX_CODES.RED.getHexcode());
+    }
+}
+
+// Automation we have store the hex code - colors
+
+enum HEX_CODES{
+    RED("#FF0000"),
+    GREEN("#61FF33"),
+    BLUE("#3377FF"),
+    YELLOW("#4477FF");
+
+    private String hexcode;
+
+    HEX_CODES(String hexcode){
+        this.hexcode = hexcode;
+    }
+
+    String getHexcode(){
+        return this.hexcode;
+    }
+
+}
