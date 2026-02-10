ec864169862ef262ecbefcdafe4022376b2bf10c
Siddharth <sidp5276@gmail.com>
2026-02-10 20:56:27 +0530

    wrapper IQ

---
+++ src/ex_23_Wrapper_class/Lab_187_IQ.java
@@ -0,0 +1,24 @@
+package ex_23_Wrapper_class;
+
+public class Lab_187_IQ {
+    public static void main(String[] args) {
+
+        Integer a = 127;
+        Integer b = 127;
+        System.out.println(a == b);       // true (cached)
+
+        Integer c = 128;
+        Integer d = 128;
+        System.out.println(c == d);    // false! (not cached)
+        System.out.println(c.equals(d));
+
+        // Java caches Integer values from -128 to 127, so == works for small numbers but fails for larger ones. Always use .equals() for wrapper comparisons.
+
+//        Integer num = null;
+//        int value = num;  // NullPointerException!
+//        System.out.println(value);
+        Integer aaa = 10;
+        System.out.println(aaa.toString());
+        System.out.println(aaa.toString() instanceof String);
+    }
+}
