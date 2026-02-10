91b5f58c32148df43954e1f78c14c7ab18eba4c5
Siddharth <sidp5276@gmail.com>
2026-02-10 20:52:49 +0530

    wrapper class

---
+++ src/ex_23_Wrapper_class/Lab_184_Wrapper_class.java
@@ -0,0 +1,13 @@
+package ex_23_Wrapper_class;
+import java.util.Collection;
+public class Lab_184_Wrapper_class {
+}
+
+class ATB1{
+    String name;
+    Long phone;
+    Integer salary;
+    Float GST;
+    Boolean isMarried;
+
+}
