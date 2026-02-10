e8ba4b25a293d363ebce69a00260508c664b8373
Siddharth <sidp5276@gmail.com>
2026-02-10 20:50:13 +0530

    wrapper conversion

---
+++ src/ex_23_Wrapper_class/Lab_183_Wrapper_conversion.java
@@ -0,0 +1,15 @@
+package ex_23_Wrapper_class;
+
+public class Lab_183_Wrapper_conversion {
+    public static void main(String[] args) {
+        int a = 10;
+        // a. // ?
+        //        a. // no attribute
+//        a.len() // no method
+        System.out.println(a);
+        Integer a1 = 10;
+        System.out.println(a1);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+    }
+}
