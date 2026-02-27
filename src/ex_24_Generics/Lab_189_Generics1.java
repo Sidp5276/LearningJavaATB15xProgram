2fbb8b5c15553c471882208198538876726e4894
Siddharth <sidp5276@gmail.com>
2026-02-27 22:16:33 +0530

    Generics

---
+++ src/ex_24_Generics/Lab_189_Generics1.java
@@ -0,0 +1,14 @@
+package ex_24_Generics;
+
+public class Lab_189_Generics1 {
+    public static void main(String[] args) {
+        temp(3, 4);
+        temp(3.34, 4.45);
+        temp("pramod", "dutta");
+    }
+
+
+    static <Pramod> Pramod temp(Pramod a, Pramod b) {
+        return null;
+    }
+}
