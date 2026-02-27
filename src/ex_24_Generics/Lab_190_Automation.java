348aa09d494ede0dc2a5f1ed77ffea3bbbfa2fac
Siddharth <sidp5276@gmail.com>
2026-02-27 22:17:41 +0530

    Generics

---
+++ src/ex_24_Generics/Lab_190_Automation.java
@@ -0,0 +1,14 @@
+package ex_24_Generics;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lab_190_Automation {
+    public static void main(String[] args) {
+        List myList = new ArrayList();
+        myList.add("pramod");
+        myList.add("Dutta");
+        myList.add(123);
+    }
+}
