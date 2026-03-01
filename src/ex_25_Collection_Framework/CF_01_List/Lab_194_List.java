8fcfb5cc374766fb0b44a13997507c45782bf947
Siddharth <sidp5276@gmail.com>
2026-03-01 21:33:01 +0530

    list

---
+++ src/ex_25_Collection_Framework/CF_01_List/Lab_194_List.java
@@ -0,0 +1,9 @@
+package ex_25_Collection_Framework.CF_01_List;
+import java.util.*;
+public class Lab_194_List {
+    public static void main(String[] args) {
+        // List s = new List()
+        List mylist0 = new ArrayList();
+        List mylist1 = new ArrayList(5);
+    }
+}
