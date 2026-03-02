415594158c96a3a8287b45bf0250b0a86b87e381
Siddharth <sidp5276@gmail.com>
2026-03-02 21:16:46 +0530

    Pojo(plain old java object)

---
+++ src/ex_25_Collection_Framework/CF_01_List/POJO.java
@@ -0,0 +1,27 @@
+package ex_25_Collection_Framework.CF_01_List;
+
+public class POJO {
+    private Integer rollNo;
+    private String name;
+
+
+    POJO() {
+        // avoid it
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(Integer rollNo) {
+        this.rollNo = rollNo;
+    }
+}
