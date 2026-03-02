94a0ca36ca64200bb8a9e66140e744c11236288d
Siddharth <sidp5276@gmail.com>
2026-03-02 21:04:28 +0530

    LinkedList

---
+++ src/ex_25_Collection_Framework/CF_01_List/Lab_201_LL_List.java
@@ -0,0 +1,16 @@
+package ex_25_Collection_Framework.CF_01_List;
+import java.util.*;
+public class Lab_201_LL_List {
+    public static void main(String[] args) {
+//        List my_list = new ArrayList();
+        List mylist = new LinkedList();
+
+        mylist.add(1);
+        mylist.add(2);
+        mylist.add(3);
+        mylist.add(4);
+        mylist.add(4);
+        System.out.println(mylist);
+
+    }
+}
