0edb45d258bf01f56e4901799512f6c73cc79d58
Siddharth <sidp5276@gmail.com>
2026-03-03 22:30:50 +0530

    Set

---
+++ src/ex_25_Collection_Framework/CF_02_SET/Lab_209_Set.java
@@ -0,0 +1,15 @@
+package ex_25_Collection_Framework.CF_02_SET;
+import java.util.*;
+public class Lab_209_Set {
+    public static void main(String[] args) {
+
+        Set hs = new HashSet();
+        hs.add("Pramod"); // da67sd7a6sdad68as67da8d67
+        hs.add("Pramod"); // da67sd7a6sdad68as67da8d67
+        hs.add("dramod"); // wewewa67sd7a668as67da8d67
+        System.out.println(hs);
+
+        Set lhs = new LinkedHashSet();
+        Set ts = new TreeSet();
+    }
+}
