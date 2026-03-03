b69aeb67446790134e30e862f88383752fddb6af
Siddharth <sidp5276@gmail.com>
2026-03-03 22:33:11 +0530

    Hash set

---
+++ src/ex_25_Collection_Framework/CF_02_SET/Lab_211_HS.java
@@ -0,0 +1,19 @@
+package ex_25_Collection_Framework.CF_02_SET;
+import java.util.*;
+public class Lab_211_HS {
+    public static void main(String[] args) {
+
+
+        Set<Integer> set = new HashSet();
+        set.add(9);
+        set.add(3);
+        set.add(1);
+        System.out.println("Set elements: " + set);
+
+
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
