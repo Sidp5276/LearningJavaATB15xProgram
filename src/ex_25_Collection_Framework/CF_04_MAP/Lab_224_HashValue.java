1395e02499c5e0bb01dbac1dc3548701539e2d49
Siddharth <sidp5276@gmail.com>
2026-03-07 23:55:37 +0530

    Hash Value

---
+++ src/ex_25_Collection_Framework/CF_04_MAP/Lab_224_HashValue.java
@@ -0,0 +1,24 @@
+package ex_25_Collection_Framework.CF_04_MAP;
+
+import java.util.Collections;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Lab_224_HashValue {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student();
+        System.out.println(s1);
+        System.out.println(s2);
+
+
+        Map m1 = new HashMap();
+        m1.put("PRamod",123);
+        m1.put(123,124);
+        m1.put(true,false);
+
+        //Collections
+    }
+}
+class Student{}
